96b04cd6333df1fa10a641e4498667d65f279016
Janara <isenalievazanara@gmail.com>
2024-08-15 21:01:51 +0600

    add

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let box = document.querySelector("box");
 let content = document.querySelector("content");
 let figers = document.querySelector("figers");
+let yin-yang = document.querySelector("yin-yang");
+// ------------------------------------------------
+
+let header = document.querySelector("header");
